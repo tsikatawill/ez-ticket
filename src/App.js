0ae3363858de93052565557e3fa986263dd82dd6
William M. Tsikata <tsikatawill@gmail.com>
2022-02-26 10:19:09 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import MovieList from "./MovieList";
-import { MovieProvider } from "./MovieListContext";
-import Nav from "./Nav";
-import AddMovie from "./AddMovie";
 
 const App = () => {
-  return (
-    <div>
-      <MovieProvider>
-        <Nav />
-        <AddMovie />
-        <MovieList />
-      </MovieProvider>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-purple": "#5267DF",
+        "primary-red": "#FA5959",
+        "primary-blue": "#242A45",
+        "primary-grey": "#9194A2",
+        "primary-white": "#f7f7f7",
+      },
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
